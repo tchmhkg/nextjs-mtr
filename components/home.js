f5c42c8a0348648a213e5bef99b718da74e66326
Truman Cheung <trumancheunghm@gmail.com>
2021-05-29 15:57:25 +0800

    - add station latlng - set nearest station on load

--- components/home.js
+++ components/home.js
@@ -1,7 +1,7 @@
 import useTranslation from '~/hooks/useTranslation';
 import styled from 'styled-components';
 import { stations } from '~/utils/next-train-data';
-import { useState } from 'react';
+import { useState, useEffect, useCallback, /*createRef*/ } from 'react';
 import Result from './train/result';
 
 const Heading = styled.h2`
@@ -27,6 +27,8 @@ const Container = styled.div`
 const SelectorWrapper = styled.div`
   display: flex;
   flex-direction: row;
+  border-radius: 8px;
+  overflow: hidden;
 `;
 
 const Left = styled.div`
@@ -37,14 +39,16 @@ const Left = styled.div`
   height: 170px;
   overflow-y: auto;
 `;
-const Right = styled(Left)``;
+const Right = styled(Left)`
+  background: ${({bgColor}) => bgColor || 'transparent'};
+`;
 
 const Option = styled.div`
   cursor: pointer;
   display: flex;
   align-items: center;
   .option-name {
-    background: ${({ color, selected, theme }) =>
+    background: ${({ color, selected }) =>
       selected ? `${color}` : 'transparent'};
     color: ${({ selected, theme }) => (selected ? '#fff' : theme.text)};
     width: 100%;
@@ -61,18 +65,11 @@ const LineOption = styled(Option)`
 
 const StationOption = styled(Option)`
   background: ${({ color }) => color};
-  &:first-child {
-    border-top-right-radius: 8px;
-  }
-  &:last-child {
-    border-bottom-right-radius: 8px;
-  }
   padding: 3px;
   .option-name {
     background: ${({ selected }) =>
       selected ? '#fff' : 'transparent'} !important;
     color: ${({ selected }) => (selected ? '#000' : '#fff')} !important;
-    border-radius: ${({ selected }) => (selected ? '8px' : 0)};
   }
 `;
 
@@ -88,6 +85,9 @@ const Home = () => {
   const { locale, t } = useTranslation();
   const [selectedStation, setSelectedStation] = useState(null);
   const [selectedLine, setSelectedLine] = useState(null);
+  const [currLocation, setCurrLocation] = useState({lat: 0, lng: 0});
+  // const [closestStation, setClosestStation] = useState({});
+  // let stationRef = [];
 
   const getLangCodeFromLocale = () => (locale === 'zh' ? 'tc' : 'en');
 
@@ -100,6 +100,78 @@ const Home = () => {
     if (!selectedLine) return [];
     return stations.find((s) => s.line.code === selectedLine);
   };
+
+  const getPositionSuccess = (pos) => {
+    var crd = pos.coords;
+  
+    // console.log('Your current position is:');
+    // console.log(`Latitude : ${crd.latitude}`);
+    // console.log(`Longitude: ${crd.longitude}`);
+    // console.log(`More or less ${crd.accuracy} meters.`);
+    setCurrLocation({lat: crd.latitude, lng: crd.longitude});
+    findNearestStation(crd.latitude, crd.longitude)
+  }
+
+  const getPositionError = (err) => {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  const getCurrLocation = () => {
+    var options = {
+      enableHighAccuracy: true,
+      maximumAge: 0
+    };
+    navigator.geolocation.getCurrentPosition(getPositionSuccess, getPositionError, options);
+  }
+
+  const findNearestStation = useCallback((lat, lng) => {
+    if(!lat || !lng) return;
+    let closest = null;
+    let closestLine = null;
+    let closestDistance = null;
+
+    for(const lineStation of stations) {
+      // console.log('LINE =>',lineStation.line.label.tc)
+      for(const station of lineStation.stations) {
+        const distance = calcDistance(lat, lng, station.location.lat, station.location.lng);
+        if(!closest || distance < closestDistance){
+          closestDistance = distance;
+          closestLine = lineStation.line?.code;
+          closest = station?.code;
+        }
+      }
+    }
+    // console.log('CLOSEST =>',closest);
+    // setClosestStation(closest);
+    setSelectedLine(closestLine);
+    setSelectedStation(closest);
+  }, [])
+
+  useEffect(() => {
+    getCurrLocation();
+  }, [])
+
+  // useEffect(() => {
+  //   console.log('scroll to ',stationRef.find())
+  //   stationRef.current?.scrollIntoView();
+  // }, [selectedStation])
+
+  const calcDistance = (lat1, lon1, lat2, lon2, unit) => {
+    var radlat1 = Math.PI * lat1/180
+    var radlat2 = Math.PI * lat2/180
+    var theta = lon1-lon2
+    var radtheta = Math.PI * theta/180
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    if (dist > 1) {
+        dist = 1;
+    }
+    dist = Math.acos(dist)
+    dist = dist * 180/Math.PI
+    dist = dist * 60 * 1.1515
+    if (unit=="K") { dist = dist * 1.609344 }
+    if (unit=="N") { dist = dist * 0.8684 }
+    return dist
+  }
 
   return (
     <Container>
@@ -122,19 +194,23 @@ const Home = () => {
             </LineOption>
           ))}
         </Left>
-        <Right>
-          {filterStations()?.stations?.map((s) => (
-            <StationOption
-              color={filterStations()?.line?.color}
-              key={s.code}
-              onClick={() => setSelectedStation(s.code)}
-              selected={s.code === selectedStation}
-            >
-              <div className="option-name station">
-                {s.label[getLangCodeFromLocale()]}
-              </div>
-            </StationOption>
-          ))}
+        <Right bgColor={filterStations()?.line?.color}>
+          {filterStations()?.stations?.map((s) => {
+            {/* const ref = createRef();
+            stationRef.push({[s.code]: ref}) */}
+            return (
+              <StationOption
+                // ref={ref}
+                key={s.code}
+                onClick={() => setSelectedStation(s.code)}
+                selected={s.code === selectedStation}
+              >
+                <div className="option-name station">
+                  {s.label[getLangCodeFromLocale()]}
+                </div>
+              </StationOption>
+            )
+          })}
         </Right>
       </SelectorWrapper>
       <Result line={selectedLine} sta={selectedStation} />
