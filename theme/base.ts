4e1265e7b1fcecc83879887c147d71f675ef0d48
tchmhkg <26540312+tchmhkg@users.noreply.github.com>
2026-02-28 03:29:07 +0800

    fix: sonarqube issues

---
+++ theme/base.ts
@@ -0,0 +1,15 @@
+const baseTheme = {
+  theme: {
+    primary: '#ff4d4f',
+    buttonText: '#ffffff',
+    chartDataZoomBackground: '#8392A5',
+    primary1: '#126AE7',
+    primary2: '#92BBF6',
+    platformBackground: '#ff4d4f',
+    platformText: '#ffffff',
+    leaving: '#ff4d4f',
+    arriving: '#52c41a',
+  },
+};
+
+export default baseTheme;
